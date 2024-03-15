26f50df75ee2cd764d71e3d01a1262f8c5da78df
Adam Erstad <adamerstad@gmail.com>
2024-03-15 07:15:03 -0700

    Board modification added, buttons used as input/output
    No game logic yet

--- script.js
+++ script.js
@@ -1,20 +1,75 @@
+function createTile() {
+    let tile = "_";
+    
+    const checkTileEmpty = () => {
+        return tile === "_";
+    };
+
+    const setTile = value => {
+        tile = value;
+    };
+
+    const getTile = () => tile;
+
+    return { 
+        setTile, getTile, checkTileEmpty,
+    };
+}
+
 const gameBoard = (function () { 
     const columns = 3;
     const rows = 3;
     const board = [];
+    let turn = "X";
+
+    const header = document.querySelector('.boardHeader');
+    const buttons = document.querySelectorAll('.boardButton');
 
     const start = () => {
-        for (x = 0; x < columns; x++) {
-            for (y = 0; y < rows; y++) {
-                board.push("_");
+        for (y = 0; y < rows; y++) {
+            board[y] = [];
+            for (x = 0; x < columns; x++) {
+                board[y].push(createTile());
             }
         }
 
+        console.log(buttons);
+        buttons.forEach(button => {
+            button.addEventListener('click', tryTile);
+        });
+
         // console.log(board);
     };
+
+    const print = () => {
+        const printableBoard = board.map(x => x.map(tile => tile.getTile()));
+        console.log(printableBoard);
+    }
+
+    const changeTurn = () => {
+        if (turn === "X") { turn = "O";}
+        else { turn = "X"; }
+        header.textContent = `${turn} Turn`;
+    }
+
+    const tryTile = e => {
+        const buttonID = parseInt(e.target.id) - 1;
+        const buttonRow = Math.floor((buttonID) / 3);
+        const buttonColumn = buttonID - (buttonRow * 3);
+
+        const tile = board[buttonRow][buttonColumn];
+        if (tile.checkTileEmpty())
+        {
+            tile.setTile(turn);
+            e.target.textContent = turn;
+            changeTurn();
+        }
+    }
     
     return {
         start,
+        print,
+        tryTile,
     };
 })();
 
